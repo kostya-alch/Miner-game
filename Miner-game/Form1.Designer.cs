e003003467f1b8d93d31c31a7e9e256a3a0c7bac
kostya-alch <kostafrompiter@gmail.com>
2022-02-10 16:34:01 +0300

    logic of winning and losing, comments, array of buttons

--- Miner-game/Form1.Designer.cs
+++ Miner-game/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(364, 371);
             this.Name = "Form1";
-            this.Text = "Сапёр";
+            this.Text = "Miner";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
--- Miner-game/Form1.cs
+++ Miner-game/Form1.cs
@@ -12,9 +12,10 @@ namespace Miner_game
 {
     public partial class Form1 : Form
     {
-        int width = 10;
-        int height = 10;
-        int distanceButtons = 35;
+        int width = 10; // ширина столбца поля
+        int height = 10; // высота столбца поля
+        int distanceButtons = 35; // расстояние между каждой кнопкой
+        ButtonExented[,] allButtons; // массив кнопок
         public Form1()
         {
             InitializeComponent();
@@ -22,16 +23,51 @@ namespace Miner_game
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            allButtons = new ButtonExented[width, height]; // экземпляр класса
+            Random rng = new Random();
             for(int x = 10; (x - 10) < width * distanceButtons; x+= distanceButtons) // цикл создания кнопок
             {
                 for (int y = 10; (y - 10) < height * distanceButtons; y+= distanceButtons)
                 {
-                    Button button = new Button(); // создание кнопки в форме.
-                    button.Location = new Point(x, y);
-                    button.Size = new Size(30, 30);
+                    ButtonExented button = new ButtonExented(); // создание кнопки в форме.
+                    button.Location = new Point(x, y); // присваиваем локацию кнопке
+                    button.Size = new Size(30, 30); // размер кнопки
+                    if (rng.Next(0, 101) < 20) // проверка на то, есть ли в кнопке бомба
+                    {
+                        button.isBomb = true;
+                    }
+                    allButtons[(x - 10) / distanceButtons, (y - 10) / distanceButtons] = button; 
                     Controls.Add(button);
+                    button.Click += new EventHandler(FieldClick);
                 }
             }
         }
+        void FieldClick(object sender, EventArgs e)
+        {
+            ButtonExented button = (ButtonExented)sender;
+            // если бомба - вызов функции взрыва, если нет - игра продолжается.
+            if (button.isBomb)
+            {
+                Explode(button);
+            }
+            else
+            {
+                EmptyField(button);
+            }
+        }
+        void Explode(ButtonExented button) // логика проигрыша если нарвался на бомбу
+        {
+            MessageBox.Show("Вы проиграли");
+            button.Text = "*";
+        }
+
+        void EmptyField(ButtonExented button) // логика если в клетке нету бомбы и игра продолжается
+        {
+            // TODO method
+        }
+    }
+    class ButtonExented: Button // наследуемый класс, добавляющий к кнопке поле бомбы.
+    {
+       public bool isBomb;
     }
 }
