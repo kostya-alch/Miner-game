7a41f1dde932d27dfca566ed7e87c379cdb9586f
kostya-alch <kostafrompiter@gmail.com>
2022-02-10 15:44:09 +0300

    buttons markup

--- Miner-game/Form1.cs
+++ Miner-game/Form1.cs
@@ -12,6 +12,9 @@ namespace Miner_game
 {
     public partial class Form1 : Form
     {
+        int width = 10;
+        int height = 10;
+        int distanceButtons = 35;
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +22,16 @@ namespace Miner_game
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            for(int x = 10; (x - 10) < width * distanceButtons; x+= distanceButtons) // цикл создания кнопок
+            {
+                for (int y = 10; (y - 10) < height * distanceButtons; y+= distanceButtons)
+                {
+                    Button button = new Button(); // создание кнопки в форме.
+                    button.Location = new Point(x, y);
+                    button.Size = new Size(30, 30);
+                    Controls.Add(button);
+                }
+            }
         }
     }
 }
